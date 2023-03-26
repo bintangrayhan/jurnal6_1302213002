c48b496d884f11c77fce532f710c057b28d9b751
PC33-IFLAB <110660092+medavidrahmadana@users.noreply.github.com>
2023-03-27 06:52:57 +0700

    Add project files.

---
+++ mo6_1302213002/SayaTubeVideo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mo6_1302213002
+{
+    internal class SayaTubeVideo
+    {
+
+    }
+}
